0e81610d10780fc13eb8613fdf85f67187b814af
Starokaznikov Oleg <starokaznikov@protei.ru>
2023-11-12 01:07:38 +0300

    сломалось

--- src/bme280_data_comp.vhd
+++ src/bme280_data_comp.vhd
@@ -7,16 +7,16 @@ entity bme280_data_comp is
 	port (
 		reset : in  std_logic;                  
 		clock : in  std_logic;                     
-		i_bme280 : in  type_inBME280;
-		o_bme280 : out type_outBME280
+		inbme280 : in  type_inBME280;
+		outbme280 : out type_outBME280
 		);
 end entity bme280_data_comp;
 
 architecture main of bme280_data_comp is
 	
-	--	constant dig_T1 : signed(15 downto 0) := to_signed(28383, 16);
-	--	constant dig_T2 : signed(15 downto 0) := to_signed(26777, 16);
-	--	constant dig_T3 : signed(15 downto 0) := to_signed(   50, 16);
+		constant dig_T1 : signed(15 downto 0) := to_signed(28383, 16);
+		constant dig_T2 : signed(15 downto 0) := to_signed(26777, 16);
+		constant dig_T3 : signed(15 downto 0) := to_signed(   50, 16);
 	
 	constant dig_P1 : signed(63 downto 0) := to_signed( 36602, 64);
 	constant dig_P2 : signed(63 downto 0) := to_signed(-10496, 64);
@@ -119,126 +119,126 @@ architecture main of bme280_data_comp is
 	
 begin
 	
-	o_bme280.P <= std_logic_vector(p);
-	o_bme280.H <= std_logic_vector(h);
+	outbme280.P <= std_logic_vector(p);
+	outbme280.T <= std_logic_vector(t);
+	outbme280.H <= std_logic_vector(h);
 	
 	process (reset,clock)
 	begin
 		if reset = '1' then
 			temp_vld_pipeline <= (others => false);
-			o_bme280.act <= false;
+			outbme280.act <= false;
 		elsif rising_edge(clock) then
-			temp_vld_pipeline <= i_bme280.act & temp_vld_pipeline(C_PIPELINE_LENGTH - 1 downto 1);
+			temp_vld_pipeline <= inbme280.act & temp_vld_pipeline(C_PIPELINE_LENGTH - 1 downto 1);
 			if temp_vld_pipeline(0) then
-				o_bme280.act <= true;
-			elsif not i_bme280.act then
-				o_bme280.act <= false;
+				outbme280.act <= true;
+			elsif not inbme280.act then
+				outbme280.act <= false;
 			end if;
 		end if;
 	end process;
 	
-	process (clock)
-	begin
-		if rising_edge(clock) then         
-			t_var1_temp1 <= shift_right(resize(signed(i_bme280.adc_T),32), 3) - signed(shift_left(resize(signed(i_bme280.dig_T(1)),32), 1));
-			t_var1_temp2 <= resize(t_var1_temp1 * signed(i_bme280.dig_T(2)), 32);
-			t_var1 <= shift_right(signed(t_var1_temp2), 11);
-			
-			t_var2_temp1 <= shift_right(resize(signed(i_bme280.adc_T),32), 4) - resize(signed(i_bme280.dig_T(1)),32);
-			t_var2_temp2 <= shift_right(resize(t_var2_temp1 * t_var2_temp1, 32), 12);
-			t_var2_temp3 <= resize(t_var2_temp2 * signed(i_bme280.dig_T(3)), 32);
-			t_var2 <= shift_right(t_var2_temp3, 14);
-			
-			t_fine <= t_var1 + t_var2;
-	--		T <= shift_right(resize(t_fine * 5, 16) + x"0080", 8);
-			o_bme280.T <= std_logic_vector(shift_right(resize(t_fine * 5, 16) + x"0080", 8));
-		end if;
-	end process;
+--	process (clock)
+--	begin
+--		if rising_edge(clock) then         
+--			t_var1_temp1 <= shift_right(signed(inbme280.adc_P), 3) - signed(shift_left(dig_T1, 1));
+--			t_var1_temp2 <= resize(t_var1_temp1 * dig_T2, 32);
+--			t_var1 <= shift_right(signed(t_var1_temp2), 11);
+--			
+--			t_var2_temp1 <= shift_right(signed(inbme280.adc_P), 4) - signed(dig_T1);
+--			t_var2_temp2 <= shift_right(resize(t_var2_temp1 * t_var2_temp1, 32), 12);
+--			t_var2_temp3 <= resize(t_var2_temp2 * dig_T3, 32);
+--			t_var2 <= shift_right(t_var2_temp3, 14);
+--			
+--			t_fine <= t_var1 + t_var2;
+--			T <= shift_right(resize(t_fine * 5, 16) + x"0080", 8);
+--		end if;
+--	end process;
 	
 	
-	process (clock)
-	begin
-		if rising_edge(clock) then
-			p_var1_temp1 <= resize(t_fine, 64) - C_PRESSURE_CONST1;
-			p_var1_temp2 <= resize(p_var1_temp1 * p_var1_temp1, 64);
-			p_var1_temp3 <= shift_right(resize(p_var1_temp2 * dig_P3, 64), 8);
-			--p_var1_temp3 <= resize(shift_right(p_var1_temp2 * dig_P3, 8), 64);
-			p_var1_temp4 <= shift_left(resize(p_var1_temp1 * dig_P2, 64), 12);
-			--p_var1_temp4 <= resize(shift_left(p_var1_temp1 * dig_P2, 12), 64);
-			p_var1_temp5 <= p_var1_temp3 + p_var1_temp4;
-			p_var1_temp6 <= shift_left(signed'(x"0000_0000_0000_0001"), 47) + p_var1_temp5;
-			p_var1       <= shift_right(resize(p_var1_temp6 * dig_P1, 64), 33);
-			--p_var1       <= resize(shift_right(p_var1_temp6 * dig_P1, 33), 64);
-			
-			p_var2_temp1 <= resize(p_var1_temp2 * dig_P6, 64);
-			p_var2_temp2 <= shift_left(resize(p_var1_temp1 * dig_P5, 64), 17);
-			p_var2_temp2 <= resize(shift_left(p_var1_temp1 * dig_P5, 17), 64);
-			p_var2_temp3 <= p_var2_temp1 + p_var2_temp2;
-			p_var2_temp4 <= shift_left(dig_P4, 35);
-			p_var2 <= p_var2_temp3 + p_var2_temp4;
-			
-			if p_var1 = signed'(x"0000_0000_0000_0000") then
-				p <= (others => '0');
-			else
-				p_temp1 <= C_PRESSURE_CONST2 - resize(signed(i_bme280.adc_P), 64);
-				p_temp2 <= shift_left(p_temp1, 31) - p_var2;
-				p_temp3 <= resize(p_temp2 * C_PRESSURE_CONST3, 64);
-				--numenator <= p_temp3;
-				--denumenator <= p_var1;
-				--p_quotient
-				var3_temp1 <= resize(shift_right(signed(p_quotient), 13) * shift_right(signed(p_quotient), 13), 64);
-				var3 <= shift_right(resize(dig_P9 * var3_temp1, 64), 25);
-				--var3 <= resize(shift_right(dig_P9 * var3_temp1, 25), 64);
-				var4 <= shift_right(resize(dig_P8 * signed(p_quotient), 64), 19);
-				--var4 <= resize(shift_right(dig_P8 * signed(p_quotient), 19), 64);
-				p_temp4 <= var3 + var4;
-				p_temp5 <= shift_right(signed(p_quotient) + p_temp4, 8);
-				p_temp6 <= p_temp5 + shift_left(dig_P7, 4);
-				p <= shift_right(resize(unsigned(std_logic_vector(p_temp6)), 16),8);
-			end if;
-			
-		end if;
-	end process;
+--	process (clock)
+--	begin
+--		if rising_edge(clock) then
+--			p_var1_temp1 <= resize(t_fine, 64) - C_PRESSURE_CONST1;
+--			p_var1_temp2 <= resize(p_var1_temp1 * p_var1_temp1, 64);
+--			p_var1_temp3 <= shift_right(resize(p_var1_temp2 * dig_P3, 64), 8);
+--			--p_var1_temp3 <= resize(shift_right(p_var1_temp2 * dig_P3, 8), 64);
+--			p_var1_temp4 <= shift_left(resize(p_var1_temp1 * dig_P2, 64), 12);
+--			--p_var1_temp4 <= resize(shift_left(p_var1_temp1 * dig_P2, 12), 64);
+--			p_var1_temp5 <= p_var1_temp3 + p_var1_temp4;
+--			p_var1_temp6 <= shift_left(signed'(x"0000_0000_0000_0001"), 47) + p_var1_temp5;
+--			p_var1       <= shift_right(resize(p_var1_temp6 * dig_P1, 64), 33);
+--			--p_var1       <= resize(shift_right(p_var1_temp6 * dig_P1, 33), 64);
+--			
+--			p_var2_temp1 <= resize(p_var1_temp2 * dig_P6, 64);
+--			p_var2_temp2 <= shift_left(resize(p_var1_temp1 * dig_P5, 64), 17);
+--			p_var2_temp2 <= resize(shift_left(p_var1_temp1 * dig_P5, 17), 64);
+--			p_var2_temp3 <= p_var2_temp1 + p_var2_temp2;
+--			p_var2_temp4 <= shift_left(dig_P4, 35);
+--			p_var2 <= p_var2_temp3 + p_var2_temp4;
+--			
+--			if p_var1 = signed'(x"0000_0000_0000_0000") then
+--				p <= (others => '0');
+--			else
+--				p_temp1 <= C_PRESSURE_CONST2 - resize(signed(inbme280.adc_P), 64);
+--				p_temp2 <= shift_left(p_temp1, 31) - p_var2;
+--				p_temp3 <= resize(p_temp2 * C_PRESSURE_CONST3, 64);
+--				--numenator <= p_temp3;
+--				--denumenator <= p_var1;
+--				--p_quotient
+--				var3_temp1 <= resize(shift_right(signed(p_quotient), 13) * shift_right(signed(p_quotient), 13), 64);
+--				var3 <= shift_right(resize(dig_P9 * var3_temp1, 64), 25);
+--				--var3 <= resize(shift_right(dig_P9 * var3_temp1, 25), 64);
+--				var4 <= shift_right(resize(dig_P8 * signed(p_quotient), 64), 19);
+--				--var4 <= resize(shift_right(dig_P8 * signed(p_quotient), 19), 64);
+--				p_temp4 <= var3 + var4;
+--				p_temp5 <= shift_right(signed(p_quotient) + p_temp4, 8);
+--				p_temp6 <= p_temp5 + shift_left(dig_P7, 4);
+--				p <= shift_right(resize(unsigned(std_logic_vector(p_temp6)), 16),8);
+--			end if;
+--			
+--		end if;
+--	end process;
 	
-	lpm_divide_s64_1 : entity work.lpm_divide_s64 
-	port map(
-		rstn => '1', clk => clock,
-		dividend => std_logic_vector(p_temp3),
-		divisor => std_logic_vector(p_var1),
-		quotient => p_quotient);
-	
-	process (clock)
-	begin
-		if rising_edge(clock) then
-			h_var1 <= t_fine - C_HUMIDITY_CONST1;
-			
-			h_var2_temp1  <= shift_left(resize(signed(i_bme280.adc_h),32), 14);
-			h_var2_temp2  <= shift_left(dig_H4, 20);
-			h_var2_temp3  <= resize(dig_H5 * h_var1, 32);
-			h_var2_temp4  <= shift_right(h_var2_temp1 - h_var2_temp2 - h_var2_temp3 + C_HUMIDITY_CONST2, 15);	   
-			h_var2_temp5  <= shift_right(resize(h_var1 * dig_H6, 32), 10);
-			h_var2_temp6  <= shift_right(resize(h_var1 * dig_H3, 32), 11);
-			h_var2_temp7  <= h_var2_temp6 + C_HUMIDITY_CONST3;
-			h_var2_temp8  <= shift_right(resize(h_var2_temp5 * h_var2_temp7, 32), 10);
-			h_var2_temp9  <= h_var2_temp8 + C_HUMIDITY_CONST5;
-			h_var2_temp10 <= resize(h_var2_temp9 * dig_H2, 32);
-			h_var2_temp11 <= shift_right(resize(h_var2_temp10 + C_HUMIDITY_CONST4, 32), 14);
-			h_var2        <= resize(h_var2_temp4 * h_var2_temp11, 32);
-			
-			h_var3_temp1 <= shift_right(h_var2, 15);
-			h_var3_temp2 <= shift_right(resize(h_var3_temp1 * h_var3_temp1, 32), 7);
-			h_var3_temp3 <= shift_right(resize(h_var3_temp2 * dig_H1, 32), 4);
-			h_var3       <= h_var2 - h_var3_temp3;
-			
-			if h_var3 < x"0000_0000" then
-				h <= (others => '0');
-			elsif h_var3 > C_HUMIDITY_CONST6 then
-				h <= resize(unsigned(std_logic_vector(shift_right(C_HUMIDITY_CONST6, 12))), 16);
-			else
-				h <= resize(unsigned(std_logic_vector(shift_right(h_var3, 12))), 16);
-			end if;
-		end if;
-	end process;
+--	lpm_divide_s64_1 : entity work.lpm_divide_s64 
+--	port map(
+--		rstn => '1', clk => clock,
+--		dividend => std_logic_vector(p_temp3),
+--		divisor => std_logic_vector(p_var1),
+--		quotient => p_quotient);
+--	
+--	process (clock)
+--	begin
+--		if rising_edge(clock) then
+--			h_var1 <= t_fine - C_HUMIDITY_CONST1;
+--			
+--			h_var2_temp1  <= shift_left(resize(signed(inbme280.adc_h),32), 14);
+--			h_var2_temp2  <= shift_left(dig_H4, 20);
+--			h_var2_temp3  <= resize(dig_H5 * h_var1, 32);
+--			h_var2_temp4  <= shift_right(h_var2_temp1 - h_var2_temp2 - h_var2_temp3 + C_HUMIDITY_CONST2, 15);	   
+--			h_var2_temp5  <= shift_right(resize(h_var1 * dig_H6, 32), 10);
+--			h_var2_temp6  <= shift_right(resize(h_var1 * dig_H3, 32), 11);
+--			h_var2_temp7  <= h_var2_temp6 + C_HUMIDITY_CONST3;
+--			h_var2_temp8  <= shift_right(resize(h_var2_temp5 * h_var2_temp7, 32), 10);
+--			h_var2_temp9  <= h_var2_temp8 + C_HUMIDITY_CONST5;
+--			h_var2_temp10 <= resize(h_var2_temp9 * dig_H2, 32);
+--			h_var2_temp11 <= shift_right(resize(h_var2_temp10 + C_HUMIDITY_CONST4, 32), 14);
+--			h_var2        <= resize(h_var2_temp4 * h_var2_temp11, 32);
+--			
+--			h_var3_temp1 <= shift_right(h_var2, 15);
+--			h_var3_temp2 <= shift_right(resize(h_var3_temp1 * h_var3_temp1, 32), 7);
+--			h_var3_temp3 <= shift_right(resize(h_var3_temp2 * dig_H1, 32), 4);
+--			h_var3       <= h_var2 - h_var3_temp3;
+--			
+--			if h_var3 < x"0000_0000" then
+--				h <= (others => '0');
+--			elsif h_var3 > C_HUMIDITY_CONST6 then
+--				h <= resize(unsigned(std_logic_vector(shift_right(C_HUMIDITY_CONST6, 12))), 16);
+--			else
+--				h <= resize(unsigned(std_logic_vector(shift_right(h_var3, 12))), 16);
+--			end if;
+--		end if;
+--	end process;
 	
 end architecture main;
 
--- src/bme280_module.vhd
+++ src/bme280_module.vhd
@@ -10,13 +10,24 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
 package bme280_lib is  	   
-	subtype type_inbme280val is std_logic_vector(31 downto 0);	 
+	subtype type_inbme280val is std_logic_vector(31 downto 0);	
+	type type_dig_P is array (9 downto 1) of std_logic_vector(15 downto 0);
+	constant clear_dig_P : type_dig_P := ((others=>(others=>'0')));	
+	type type_dig_T is array (3 downto 1) of std_logic_vector(15 downto 0);
+	constant clear_dig_T : type_dig_T := ((others=>(others=>'0')));	
+	type type_dig_H is array (6 downto 1) of std_logic_vector(15 downto 0);
+	constant clear_dig_H : type_dig_H := ((others=>(others=>'0')));	
 	type type_inBME280 is record
-		act : std_logic;
-		P : std_logic_vector(31 downto 0);
-		T : std_logic_vector(31 downto 0);
-		H : std_logic_vector(31 downto 0);
-	end record;
+		act : boolean;
+		adc_P : std_logic_vector(19 downto 0);
+		dig_P : type_dig_P;
+		adc_T : std_logic_vector(19 downto 0);
+		dig_T : type_dig_T;
+		adc_H : std_logic_vector(15 downto 0);
+		dig_H : type_dig_H;
+	end record;	
+	constant clear_inbme280 : type_inBME280 := (false,(others=>'0'),clear_dig_P,(others=>'0'),clear_dig_T,(others=>'0'),clear_dig_H);	
+	
 	constant max_bme280val : integer :=16; 	  
 	subtype type_bme280val is std_logic_vector(max_bme280val-1 downto 0);	 
 	type type_outBME280 is record
@@ -25,7 +36,7 @@ package bme280_lib is
 		T : type_bme280val;
 		H : type_bme280val;
 	end record;
-	constant clear_bme280 : type_outBME280 := (false,(others=>'0'),(others=>'0'),(others=>'0'));	
+	constant clear_outbme280 : type_outBME280 := (false,(others=>'0'),(others=>'0'),(others=>'0'));	
 end bme280_lib;
 -------------------------------------------------------------------------------
 library IEEE;
@@ -42,7 +53,7 @@ entity bme280_module is
 	generic( ref_freq : integer:=125000000 );
 	port( reset,clock: in std_logic; 
 		scl,sda: inout std_logic;
-		BME280: out type_outBME280 );
+		bme280: out type_outBME280 );
 end bme280_module;
 
 architecture main of bme280_module is			 
@@ -139,29 +150,30 @@ architecture main of bme280_module is
 	signal i2c_data_mem : type_mem_i2c;
 	signal i2c_q : type_data_i2c;
 	
-	signal dig_T1,dig_T2,dig_T3: std_logic_vector(15 downto 0); 
-	signal adc_T: std_logic_vector(19 downto 0); 
+--	signal dig_T1,dig_T2,dig_T3: std_logic_vector(15 downto 0); 
+--	signal adc_T: std_logic_vector(19 downto 0); 
 --	signal T_var2d: std_logic_vector(15 downto 0); 
 --	signal T_var1,T_var1b,T_var2,T_var2b,T_var2c: std_logic_vector(31 downto 0); 
 --	signal T_var1a,T_var2a,Ta: std_logic_vector(47 downto 0); 
 --	signal t_fine: std_logic_vector(31 downto 0); 
 --	signal T: std_logic_vector(15 downto 0); 	
 	
-	signal dig_P1,dig_P2,dig_P3,dig_P4,dig_P5,dig_P6,dig_P7,dig_P8,dig_P9: std_logic_vector(15 downto 0); 
-	signal adc_P: std_logic_vector(19 downto 0); 
+--	signal dig_P1,dig_P2,dig_P3,dig_P4,dig_P5,dig_P6,dig_P7,dig_P8,dig_P9: std_logic_vector(15 downto 0); 
+--	signal adc_P: std_logic_vector(19 downto 0); 
 --	signal P_var1a: std_logic_vector(31 downto 0); 
 --	signal P_varXa: std_logic_vector(47 downto 0); 
 --	signal P_var2b,P_var2c: std_logic_vector(51 downto 0); 
 --	signal P_var2a: std_logic_vector(63 downto 0); 
 	
-	signal dig_H1,dig_H3,dig_H6: std_logic_vector(7 downto 0); 
-	signal dig_H4,dig_H5: std_logic_vector(11 downto 0); 
-	signal dig_H2: std_logic_vector(15 downto 0); 
-	signal adc_H: std_logic_vector(15 downto 0); 
+--	signal dig_H1,dig_H3,dig_H6: std_logic_vector(7 downto 0); 
+--	signal dig_H4,dig_H5: std_logic_vector(11 downto 0); 
+--	signal dig_H2: std_logic_vector(15 downto 0); 
+--	signal adc_H: std_logic_vector(15 downto 0); 
 --	signal H_var1: std_logic_vector(31 downto 0); 
 	
 	signal present: std_logic;
-	signal inBME280,outBME280: type_inBME280; 
+	signal inBME280: type_inBME280; 
+--	signal outBME280: type_outBME280; 
 	
 begin
 --	--------------------------------------------
@@ -255,60 +267,54 @@ begin
 	begin
 		if reset='1' then 	
 			present<='0';
-			BME280<=clear_bme280; 
+			inBME280<=clear_inbme280; 
 		elsif rising_edge(clock) and ena then 
-			BME280.act<=present='1'; 
-			if outBME280.act='1' then	
-				BME280.P<=outBME280.P(15 downto 0); 
-				BME280.T<=outBME280.T(15 downto 0); 
-				BME280.H<=outBME280.H(15 downto 0); 
-			end if;
 			
 			if i2c_valid and conv_integer(i2c_adr_mem)=adr_bme280 then	present<=boolean_to_data(i2c_q=bme280_ID); end if;
 			--------------------------------------------	
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digT1_h then	dig_T1(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digT1_l then	dig_T1(7 downto 0)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digT2_h then	dig_T2(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digT2_l then	dig_T2(7 downto 0)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digT3_h then	dig_T3(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digT3_l then	dig_T3(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digT1_h then	inBME280.dig_T(1)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digT1_l then	inBME280.dig_T(1)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digT2_h then	inBME280.dig_T(2)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digT2_l then	inBME280.dig_T(2)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digT3_h then	inBME280.dig_T(3)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digT3_l then	inBME280.dig_T(3)(7 downto 0)<=i2c_q; end if;
 			--------------------------------------------	
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP1_h then	dig_P1(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP1_l then	dig_P1(7 downto 0)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP2_h then	dig_P2(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP2_l then	dig_P2(7 downto 0)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP3_h then	dig_P3(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP3_l then	dig_P3(7 downto 0)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP4_h then	dig_P4(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP4_l then	dig_P4(7 downto 0)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP5_h then	dig_P5(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP5_l then	dig_P5(7 downto 0)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP6_h then	dig_P6(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP6_l then	dig_P6(7 downto 0)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP7_h then	dig_P7(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP7_l then	dig_P7(7 downto 0)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP8_h then	dig_P8(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP8_l then	dig_P8(7 downto 0)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP9_h then	dig_P9(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP9_l then	dig_P9(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP1_h then	inBME280.dig_P(1)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP1_l then	inBME280.dig_P(1)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP2_h then	inBME280.dig_P(2)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP2_l then	inBME280.dig_P(2)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP3_h then	inBME280.dig_P(3)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP3_l then	inBME280.dig_P(3)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP4_h then	inBME280.dig_P(4)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP4_l then	inBME280.dig_P(4)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP5_h then	inBME280.dig_P(5)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP5_l then	inBME280.dig_P(5)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP6_h then	inBME280.dig_P(6)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP6_l then	inBME280.dig_P(6)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP7_h then	inBME280.dig_P(7)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP7_l then	inBME280.dig_P(7)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP8_h then	inBME280.dig_P(8)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP8_l then	inBME280.dig_P(8)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP9_h then	inBME280.dig_P(9)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digP9_l then	inBME280.dig_P(9)(7 downto 0)<=i2c_q; end if;
 			--------------------------------------------	
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH1 then	dig_H1<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH2_h then	dig_H2(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH2_l then	dig_H2(7 downto 0)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH3 then	dig_H3<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH4 then	dig_H4(11 downto 4)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH45 then	dig_H4(3 downto 0)<=i2c_q(3 downto 0); dig_H5(3 downto 0)<=i2c_q(7 downto 4); end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH5 then	dig_H5(11 downto 4)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH6 then	dig_H6<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH1 then	inBME280.dig_H(1)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH2_h then	inBME280.dig_H(2)(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH2_l then	inBME280.dig_H(2)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH3 then	inBME280.dig_H(3)(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH4 then	inBME280.dig_H(4)(11 downto 4)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH45 then	inBME280.dig_H(4)(3 downto 0)<=i2c_q(3 downto 0); inBME280.dig_H(5)(3 downto 0)<=i2c_q(7 downto 4); end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH5 then	inBME280.dig_H(5)(11 downto 4)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_digH6 then	inBME280.dig_H(6)(7 downto 0)<=i2c_q; end if;
 			--------------------------------------------	
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_press2 then adc_P(19 downto 12)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_press1 then adc_P(11 downto 4)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_press0 then adc_P(3 downto 0)<=i2c_q(7 downto 4); end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_temp2 then adc_T(19 downto 12)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_temp1 then adc_T(11 downto 4)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_temp0 then adc_T(3 downto 0)<=i2c_q(7 downto 4); end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_hum1 then adc_H(15 downto 8)<=i2c_q; end if;
-			if i2c_valid and conv_integer(i2c_adr_mem)=adr_hum0 then adc_H(7 downto 0)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_press2 then inBME280.adc_P(19 downto 12)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_press1 then inBME280.adc_P(11 downto 4)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_press0 then inBME280.adc_P(3 downto 0)<=i2c_q(7 downto 4); end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_temp2 then inBME280.adc_T(19 downto 12)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_temp1 then inBME280.adc_T(11 downto 4)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_temp0 then inBME280.adc_T(3 downto 0)<=i2c_q(7 downto 4); end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_hum1 then inBME280.adc_H(15 downto 8)<=i2c_q; end if;
+			if i2c_valid and conv_integer(i2c_adr_mem)=adr_hum0 then inBME280.adc_H(7 downto 0)<=i2c_q; end if;
 		end if;
 	end process main_proc; 
 	--------------------------------------------	
@@ -335,21 +341,9 @@ begin
 		i2c_q => i2c_q
 		);
 	
-	inBME280.act<=present;
-	inBME280.P<=ext(adc_P,32);
-	inBME280.T<=ext(adc_T,32);
-	inBME280.H<=ext(adc_H,32);
 	bme280_data_calc1 : entity work.bme280_data_comp 
-	port map(
-		i_rst => reset,
-		i_clk => clock,
-		i_adc_T => inBME280.T,
-		i_adc_P => inBME280.P,
-		i_adc_H => inBME280.H,
-		i_adc_vld => inBME280.act,
-		o_temperature => outBME280.T,
-		o_pressure => outBME280.P,
-		o_humidity => outBME280.H,
-		o_valid => outBME280.act
+	port map( reset => reset, clock => clock,
+		inBME280 => inBME280,
+		outBME280 => bme280
 		);
 end main; 
